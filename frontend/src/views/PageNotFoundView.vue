2178b6d9ac8b0f0c7378c75bd09b2ce550435ed9
José Miguel Pedraza Mateo <jmpmateo3042013@gmail.com>
2023-02-21 02:11:39 -0500

    404 page

---
+++ frontend/src/views/PageNotFoundView.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+function goHome() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div style="height: 100vh; width: 100vw;" class="d-flex flex-column justify-center align-center">
+    <h1 class="mb-2">Page not found.</h1>
+    <v-btn color="blue" @click.prevent="goHome">Return to home</v-btn>
+  </div>
+</template>
+
+<style></style>
