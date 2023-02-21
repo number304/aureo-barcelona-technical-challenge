5a23ee85d8e53cdd483d7aa24299a2dacaef96db
José Miguel Pedraza Mateo <jmpmateo3042013@gmail.com>
2023-02-21 02:09:39 -0500

    refresh login with JWT logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import { RouterView, useRouter } from 'vue-router'
+import { useUserStore } from './stores/user';
+import apiClient from './utils/axiosClient';
+
+const userStore = useUserStore()
+const router = useRouter()
+
+onMounted(async () => {
+  await apiClient('auth/refresh-signin')
+    .then(res => {
+      userStore.user = res.data
+      router.push('/characters')
+    })
+    .catch((err) => console.error(err))
+})
 </script>
 
 <template>
